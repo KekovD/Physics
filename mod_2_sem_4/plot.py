0a039f5959d5671b91e4dcf0a35b4666e1cf541b
Dmitry Lavrenov <lavrenov-2015@bk.ru>
2024-05-19 22:50:22 +0300

    feat: add README.md

--- mod_2_sem_4/plot.py
+++ mod_2_sem_4/plot.py
@@ -2,24 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-# Задаем параметры дифракционной решетки
 period = 1.0  # период решетки
 total_strokes = 800  # общее число штрихов
 
-# Создаем координатную сетку
-theta = np.linspace(-np.pi / 2, np.pi / 2, 100)  # угол дифракции
-wavelength = np.linspace(400, 750, 2000)  # длина волны в нм
-
+theta = np.linspace(-np.pi / 2, np.pi / 2, 100)     # угол дифракции
+wavelength = np.linspace(400, 750, 1000)  # длина волны в нм
 Theta, Wavelength = np.meshgrid(theta, wavelength)
 
 # Расчет интенсивности с использованием формулы для дифракции
 Intensity = np.sin(np.pi * total_strokes * period * np.sin(Theta) / Wavelength) ** 2 / \
             (np.sin(np.pi * period * np.sin(Theta) / Wavelength) ** 2)
 
-# Нормализуем интенсивность для использования в качестве параметра alpha
+
 Intensity_normalized = Intensity / np.max(Intensity)
 
-# Создаем цветовую карту для видимого спектра с учетом прозрачности
 cmap = LinearSegmentedColormap.from_list(
     "wavelength_colormap",
     [(0.0, "violet"), (0.15, "indigo"), (0.3, "blue"), (0.45, "cyan"),
@@ -27,13 +23,9 @@ cmap = LinearSegmentedColormap.from_list(
 )
 
 
-# Функция для получения RGBA цветов с учетом alpha и прозрачности для вне диапазона
 def get_rgba_colors(wavelength, cmap, alpha):
-    # Нормализуем длины волн в диапазоне 400-700 нм
     norm_wavelength = (wavelength - 400) / (700 - 400)
-    # Применяем цветовую карту к нормализованным значениям длин волн
     colors = cmap(norm_wavelength)
-    # Применяем прозрачность для значений вне диапазона
     colors[..., -1] = np.where((wavelength < 400) | (wavelength > 700), 0, alpha)
     return colors
 
